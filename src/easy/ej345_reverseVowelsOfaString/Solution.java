eef37ba2c069a86536c51e66ff3bdaae48cd74bb
Nahuel <nasosa@frba.utn.edu.ar>
2025-06-24 15:36:18 -0300

    exercise 345 (reverseVowels)

---
+++ src/easy/ej345_reverseVowelsOfaString/Solution.java
@@ -0,0 +1,40 @@
+package easy.ej345_reverseVowelsOfaString;
+
+// Problema: Reverse Vowels of a String
+// Enlace: https://leetcode.com/problems/reverse-vowels-of-a-string
+
+public class Solution {
+    private static boolean isVowel(char c) {
+        return "aeiouAEIOU".indexOf(c) != -1;
+    }
+
+    public static String reverseVowels(String s) {
+        char[] out = s.toCharArray();
+        int lastIndex = out.length-1;
+
+        for (int i = 0; i < lastIndex; i++) {
+            if (isVowel(out[i])) {
+                for (int j = lastIndex; j > i ; j--) {
+                    if (isVowel(out[j])) {
+                        char aux = out[i];
+                        out[i] = out[j];
+                        out[j] = aux;
+                        lastIndex = j - 1;
+                        break;
+                    }
+                }
+            }
+        }
+        return new String(out);
+    }
+
+    public static void main(String[] args) {
+        String s = "IceCreAm";
+        String s2 = "leetcode";
+
+        // Output: "AceCreIm"
+        System.out.println(reverseVowels(s));
+        // Output: "leotcede"
+        System.out.println(reverseVowels(s2));
+    }
+}
