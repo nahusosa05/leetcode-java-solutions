2a3ab764a65e1fbe1f97a7a14eec0335e8c9c56c
Nahuel Sosa <nasosa@frba.utn.edu.ar>
2025-06-26 23:46:16 -0300

    exercise 334 (Increasing Triplet Subsequence)

---
+++ src/medium/ej334_IncreasingTripletSubsequence/Solution.java
@@ -0,0 +1,29 @@
+package medium.ej334_IncreasingTripletSubsequence;
+
+public class Solution {
+    public static boolean increasingTriplet(int[] nums) {
+        // variables para retener valores
+        int firstLowerValue = Integer.MAX_VALUE;
+        int secondLowerValue = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            // num es menor que el primer guardado? si es así lo guardo en el.
+            if (num <= firstLowerValue) {
+                firstLowerValue = num;
+            // num es menor que el segundo guardado? si es así lo guardo en el.
+            } else if (num <= secondLowerValue) {
+                secondLowerValue = num;
+            // si no, es mayor que los 2 (first,second)
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {5,4,3,2,1};
+        // display True
+        System.out.println(increasingTriplet(nums));
+    }
+}
