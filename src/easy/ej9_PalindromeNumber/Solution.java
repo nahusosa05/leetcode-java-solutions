c13a9502a74e1ef94632f1ff775c19f814e6d15e
Nahuel Sosa <nasosa@frba.utn.edu.ar>
2025-07-07 23:48:35 -0300

    exercise 9 (PalidromeNumber) in process

---
+++ src/easy/ej9_PalindromeNumber/Solution.java
@@ -0,0 +1,13 @@
+package easy.ej9_PalindromeNumber;
+
+public class Solution {
+    public static boolean isPalindrome(int x) {
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121)); // true
+        System.out.println(isPalindrome(-121)); // false
+        System.out.println(isPalindrome(10)); // false
+    }
+}
